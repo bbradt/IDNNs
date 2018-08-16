5494fa5a38993146262da2d74bc763ca8e6ce3b7
Bradley Baker <bbradt@gmail.com>
2018-08-16 12:20:39 -0600

    fake data

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+declare -a learning_rates=(0.0001)
+
+for i in "${learning_rates[@]}"
+do
+    echo "LEARNING RATE"
+    echo $i
+    python main.py -learning_rate $i -num_epochs 8000 -num_repeat 1 -d_name fake
+done
